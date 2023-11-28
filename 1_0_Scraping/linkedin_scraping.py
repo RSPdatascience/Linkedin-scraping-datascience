f1d4528388de9184579a739cef50822ee46ba2b3
RSPdatascience <151466572+RSPdatascience@users.noreply.github.com>
2023-11-28 12:05:14 +0100

    first commit

---
+++ 1_0_Scraping/linkedin_scraping.py
@@ -0,0 +1,109 @@
+# This is Linkedin job offer scraper
+# This code saves only job offers that contain a particular keyword
+# For the code to work, open the Chrome browser from CMD with this command:
+# "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222
+# (Modify the route or the port if necessary)
+# Then open linkedin/jobs and perform a job search
+# Scroll down the list of job offers with the slider, so that all offers are loaded
+# Run this code and wait for all the offers to be read
+# Go to the next page with offers, scroll down again and run the code again
+# Repeat untill all offers are read
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import re
+
+
+remote_debugging_port = 9222  # Update with the correct port number
+
+# Create a ChromeOptions object and specify the debugging address
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("debuggerAddress", f"localhost:{remote_debugging_port}")
+
+# Disable popup blocking
+chrome_options.add_argument("--disable-popup-blocking")
+
+# Initialize a web driver to connect to the currently open browser
+driver = webdriver.Chrome(options=chrome_options)
+
+# Get the current page's HTML content
+html_content = driver.page_source
+
+# Parse the HTML content with Beautiful Soup
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Extract and print job titles and company names on the current page
+job_card_elements = soup.find_all("div", class_="job-card-container")
+new_data = set()  # Store new data to be written to the file
+
+# Keyword to filter job titles
+keyword = 'data'
+
+for job_card_element in job_card_elements:
+    job_title_element = job_card_element.find("a", class_="job-card-container__link")
+    company_name_element = job_card_element.find("span", class_="job-card-container__primary-description")
+    location_element = job_card_element.find("li", class_="job-card-container__metadata-item")
+
+    if job_title_element and company_name_element:
+        job_title = job_title_element.text.strip()
+        company_name = company_name_element.text.strip()
+        location = location_element.text.strip()
+        
+        # Check if the keyword is present in the job title
+        if keyword.lower() in job_title.lower():
+            print('Job title: ', job_title)
+            print('Company name: ', company_name)
+            print('Location: ', location)
+
+
+            # Click on the job title to open the job offer page in a new window using JavaScript
+            job_title_element_href = job_title_element['href']
+            job_url = f"https://www.linkedin.com{job_title_element_href}"
+
+            # Use JavaScript to open the link in a new window
+            driver.execute_script(f"window.open('{job_url}');")
+
+            # Switch to the new tab
+            driver.switch_to.window(driver.window_handles[1])
+
+            try:
+                # Wait for the job offer page to load (adjust the timeout as needed)
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "jobs-description-content__text"))
+                )
+
+                # Extract job description from the job offer page
+                job_offer_soup = BeautifulSoup(driver.page_source, 'html.parser')
+                job_description_element = job_offer_soup.find("div", class_="jobs-description-content__text")
+
+                
+                if job_description_element:
+                    job_description = job_description_element.text.strip()[19:]
+                    #job_description = re.sub(r'[^\x00-\x7FáéíóúÁÉÍÓÚüÜñÑ●•·∙⋅\'’“”.,;:!?]+', '\n', job_description)    #removes unicode symbols
+                    job_description = job_description.replace('\n', ' ') 
+                    #job_description = re.sub(' +', ' ', job_description) # removes series of spaces
+
+                    print("Job Description:", job_description)
+                    new_data.add((job_title, company_name, location, job_description))
+
+            except Exception as e:
+                print(f"Error waiting for element on job offer page: {e}")
+
+            # Close the current tab (job offer page)
+            driver.close()
+
+            # Switch back to the main window
+            driver.switch_to.window(driver.window_handles[0])
+            
+            print()
+            
+            with open('../1_1_Data/job_offers_datascience.txt', 'a', encoding='utf-8') as file:
+                file.write(f"{job_title}\t{company_name}\t{location}\t{job_description}\n")
+           
+            print()
+
+# Close the web driver when you're done
+driver.quit()
---
+++ README.md
@@ -0,0 +1,21 @@
+# Linkedin scraping and datascience
+
+![Histogram](histogram.PNG)
+
+### Summary
+Linkedin's job offer search engine does not filter correctly and returns many irrelevant offers. The scraper included in this repository allows one to extract only relevant offers. Furthermore, the offers are analyzed to find and quantify some relevant data.
+
+### The scraper
+The scraper _linkedin_scraping.py_ can be found in the folder _1_0_Scraping_. It collects offers within a Linkedin/jobs search and filters them by a keyword. The job offers together with their titles, company names and location are stored in a tab-separated text file, located in the _1_1_Data folder_.
+
+### Data analysis
+The Jupyter Notebook LinkedinOffers.ipynb located in the _2_DataAnalysis_ folder reads the text file and generantes a Pandas dataframe. Then it performs the following operations on the data:
+
+- Counts the number of offers per company.
+- Creates a job-offer similarity matrix for a given company.
+- Finds, counts and plots in a histogram some common terms corresponding to data science tools.
+- Finds job offers with no common terms (job offers that are possibly unrelated to data science).
+- Finds and determines the number and percentage of job offers that imply hybrid or remote work.
+- Reads the job offers to find the years of experience required where specified.
+- Reads the job offers to find the salary where specified.
+- Finds other user-specified terms and their context.
